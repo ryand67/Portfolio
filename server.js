9f61fc468f7b89b105d53d46818a9223c7e49e8c
ryand67 <rwdevenney@gmail.com>
2020-07-05 17:33:53 -0400

    server

--- server.js
+++ server.js
@@ -5,14 +5,12 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-const publicPath = path.join(__dirname,'..','public');
-
-app.use(express.static(publicPath));
+app.use(express.static('./public'));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(publicPath, 'index.html'));
+    res.sendFile(path.join('./public/index.html'));
 })
 
-app.listen(PORT, (err) => {
+app.listen(PORT, () => {
     console.log('Connected')
 })
