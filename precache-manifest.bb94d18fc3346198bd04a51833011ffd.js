57b4201334cfd77669b843a8dd8901bec13e6bfe
ryand67 <rwdevenney@gmail.com>
2020-07-05 18:35:06 -0400

    Updates

---
+++ precache-manifest.bb94d18fc3346198bd04a51833011ffd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2310f5fa6f301240d14bbeb2852f6b16",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "874edf68f5245bb989ca",
+    "url": "/portfolio/static/css/main.f0b916dd.chunk.css"
+  },
+  {
+    "revision": "b42700e71a8277ab533b",
+    "url": "/portfolio/static/js/2.056a8520.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.056a8520.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "874edf68f5245bb989ca",
+    "url": "/portfolio/static/js/main.73e89b95.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "63e337120852bb989f1053c3c01ef8d1",
+    "url": "/portfolio/static/media/profilePic.63e33712.jpg"
+  }
+]);
