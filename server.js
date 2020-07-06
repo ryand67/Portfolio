04846cbcd36caae32f6c9d5821cde80007e8d948
ryand67 <rwdevenney@gmail.com>
2020-07-05 20:03:30 -0400

    changes

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-// const express = require('express');
-// const path = require('path');
+const express = require('express');
+const path = require('path');
 
-// const app = express();
+const app = express();
 
-// const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-// app.use(express.static('./public'));
+app.use(express.static('./build'));
 
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join('./public/index.html'));
-// })
+app.get('*', (req, res) => {
+    res.sendFile(path.join('./public/index.html'));
+})
 
-// app.listen(PORT, () => {
-//     console.log('Connected')
-// })
+app.listen(PORT, () => {
+    console.log('Connected')
+})
