62991bb7973de20b3cd619d2aa6fdd1534e9608c
ryand67 <rwdevenney@gmail.com>
2020-08-14 17:11:03 -0400

    updated picture in works

--- src/components/pages/Work.js
+++ src/components/pages/Work.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import WorkCard from '../WorkCard'
 import './work.css';
-import work1 from './trailheadsScreenShot.jpg';
+import work1 from './innovationScreengrab.jpg';
 import work2 from './squawkrPicture.png';
 import work3 from './weatherScreenShot.png';
 
@@ -10,7 +10,7 @@ const Work = () => {
         <main className="workPage">
             <h1 className="workTitle">Work</h1>
             <div className="workGallery">
-                <WorkCard imgSrc={work1} gitHub="https://github.com/lu-gflores/TrailHeads" deployed="https://lu-gflores.github.io/TrailHeads/" alt="Trailheads Screenshot"/>
+                <WorkCard imgSrc={work1} gitHub="https://github.com/JRivera-31/Innovation-Auto-Repair" deployed="https://innovation-auto-repair.vercel.app/" alt="Innovation Auto Parts Screenshot"/>
                 <WorkCard imgSrc={work2} gitHub="https://github.com/ryand67/Squawkr" deployed="https://squawkr-uncc.herokuapp.com/" alt="Squawkr Screenshot" />
                 <WorkCard imgSrc={work3} gitHub="https://github.com/ryand67/Weather-Dashboard" deployed="https://ryand67.github.io/Weather-Dashboard/" alt="Weather Dashboard Link"/>
             </div>
