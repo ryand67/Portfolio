4b79e866a033b1cb1b25b7532a456e5a657ad282
ryand67 <rwdevenney@gmail.com>
2020-08-22 13:15:50 -0400

    server changes

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 const express = require('express');
+const morgan = require('morgan');
 const path = require('path');
-const PORT = process.env.PORT || 3001;
-
 const app = express();
+const PORT = process.env.PORT || 3001;
+const cors = require('cors');
+
+app.use(morgan('dev'));
+app.use(cors());
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname+'client/build/index.html'));
+    res.sendFile(path.join(__dirname, 'client/build/index.html'));
 })
 
 app.listen(PORT, () => {
-    console.log(`LISTENING ON ${PORT}`)
-})
+    console.log(`Listening on ${PORT}`)
+});
