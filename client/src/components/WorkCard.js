39ea2208824114917c05497465ac3d198d0284cf
ryand67 <rwdevenney@gmail.com>
2020-08-24 22:07:34 -0400

    work responsiveness

--- client/src/components/WorkCard.js
+++ client/src/components/WorkCard.js
@@ -5,6 +5,7 @@ const WorkCard = (props) => {
     return (
         <div className="workCard">
             <a href={props.deployed} target="_blank"><img src={props.imgSrc} alt={props.alt} className="workImg"/></a>
+            <h3 className="workTitles">{props.title}</h3>
             <p className="workDesc">{props.desc}</p>
             <a href={props.gitHub} target="_blank" className="gitHubLink">GitHub Repo</a>
         </div>
--- client/src/components/pages/Work.js
+++ client/src/components/pages/Work.js
@@ -11,13 +11,13 @@ const Work = () => {
         <main className="workPage">
             <h1 className="workTitle">Work</h1>
             <div className="workGallery">
-                <WorkCard imgSrc={work1} gitHub="https://github.com/JRivera-31/Innovation-Auto-Repair" deployed="https://innovation-auto-repair.vercel.app/" alt="Innovation Auto Parts Screenshot" desc="An app for an auto repair company in Texas that allows customers to book appointments, and allows the owners to manage appointments, and available dates."/>
-                <WorkCard imgSrc={work2} gitHub="https://github.com/ryand67/Squawkr" deployed="https://squawkr-uncc.herokuapp.com/" alt="Squawkr Screenshot" desc="A simple message board/social media platform for people to share ideas with others." />
+                <WorkCard imgSrc={work1} gitHub="https://github.com/JRivera-31/Innovation-Auto-Repair" deployed="https://innovation-auto-repair.vercel.app/" title="Innovation Auto Repair" alt="Innovation Auto Repair Screenshot" desc="An app for an auto repair company in Texas that allows customers to book appointments, and allows the owners to manage appointments, and available dates."/>
+                <WorkCard imgSrc={work2} gitHub="https://github.com/ryand67/Squawkr" deployed="https://squawkr-uncc.herokuapp.com/" alt="Squawkr Screenshot" title="Squawkr" desc="A simple message board/social media platform for people to share ideas with others." />
                 
             </div>
             <div className="workGallery workGalleryBottom">
-                <WorkCard imgSrc={work3} gitHub="https://github.com/ryand67/Weather-Dashboard" deployed="https://ryand67.github.io/Weather-Dashboard/" alt="Weather Dashboard Link" desc="An app that allows users to check the current weather of any city and receive a 5 day forecast." />
-                <WorkCard imgSrc={work4} deployed="http://tsg-live-preview.herokuapp.com/" gitHub="https://github.com/ryand67/TSG-Site" desc="A site to promote the services of the travel agency TravelSalesGroup." />
+                <WorkCard imgSrc={work3} gitHub="https://github.com/ryand67/Weather-Dashboard" deployed="https://ryand67.github.io/Weather-Dashboard/" title="Weather Dashboard" alt="Weather Dashboard Link" desc="An app that allows users to check the current weather of any city and receive a 5 day forecast." />
+                <WorkCard imgSrc={work4} deployed="http://tsg-live-preview.herokuapp.com/" title="TravelSalesGroup" gitHub="https://github.com/ryand67/TSG-Site" desc="[Under Construction] A site to promote the services of the travel agency TravelSalesGroup." />
             </div>
         </main>
     )
