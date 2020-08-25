7477a67f0ad7168bda3d8461b3087c730a54ca11
ryand67 <rwdevenney@gmail.com>
2020-08-25 16:56:59 -0400

    title spacing on work

--- client/src/components/pages/Work.js
+++ client/src/components/pages/Work.js
@@ -13,7 +13,6 @@ const Work = () => {
             <div className="workGallery">
                 <WorkCard imgSrc={work1} gitHub="https://github.com/JRivera-31/Innovation-Auto-Repair" deployed="https://innovation-auto-repair.vercel.app/" title="Innovation Auto Repair" alt="Innovation Auto Repair Screenshot" desc="An app for an auto repair company in Texas that allows customers to book appointments, and allows the owners to manage appointments, and available dates."/>
                 <WorkCard imgSrc={work2} gitHub="https://github.com/ryand67/Squawkr" deployed="https://squawkr-uncc.herokuapp.com/" alt="Squawkr Screenshot" title="Squawkr" desc="A simple message board/social media platform for people to share ideas with others." />
-                
             </div>
             <div className="workGallery workGalleryBottom">
                 <WorkCard imgSrc={work3} gitHub="https://github.com/ryand67/Weather-Dashboard" deployed="https://ryand67.github.io/Weather-Dashboard/" title="Weather Dashboard" alt="Weather Dashboard Link" desc="An app that allows users to check the current weather of any city and receive a 5 day forecast." />
