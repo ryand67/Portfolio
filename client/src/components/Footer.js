c53c22a5450de4d8c0d9194a655ef68876c3f9d2
ryand67 <rwdevenney@gmail.com>
2020-08-25 13:11:11 -0400

    footer reorganize

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -7,14 +7,14 @@ const Footer = () => {
             <a className="footerLink" href="https://github.com/ryand67" target="_blank">
                 <i className="fab click-icon fa-github fa-2x"></i>
             </a>
-            <a className="footerLink" href="http:// open.spotify.com/artist/0FARTTlpzuvZOT6mf2hSc4?si=PyUmcw0_QsaskTTEr87Olg" target="_blank">
-                <i className="fab click-icon fa-spotify fa-2x"></i>
-            </a>
             <a className="footerLink" href="https://www.instagram.com/ryan_devenney67/" target="_blank">
                 <i className="fab click-icon fa-instagram fa-2x"></i>
             </a>
             <a className="footerLink" href="https://www.linkedin.com/in/ryandevenney67/" target="_blank">
                 <i className="fab click-icon fa-linkedin fa-2x"></i>
+            </a>
+            <a className="footerLink" href="http:// open.spotify.com/artist/0FARTTlpzuvZOT6mf2hSc4?si=PyUmcw0_QsaskTTEr87Olg" target="_blank">
+                <i className="fab click-icon fa-spotify fa-2x"></i>
             </a>
         </footer>
     )
