a0e636382d556e3c2a1ba6cc5a80297fef4644a5
ryand67 <rwdevenney@gmail.com>
2021-01-22 11:49:34 -0500

    initial

---
+++ client/pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Ryan Devenney</title>
+        <link rel="icon" href='Favicon.png' />
+      </Head>
+
+      <main className={styles.main}>
+        
+      </main>
+    </div>
+  )
+}
