1bc4c2e7d3c850e1c2b7acd4aa5978c68daccc14
ryand67 <rwdevenney@gmail.com>
2021-01-22 12:32:21 -0500

    footer and main title

---
+++ client/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './footer.module.css';
+
+export default function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <section className={styles.footerIcons}>
+                <a><i className={`${styles.footerIcon} fab fa-github`}></i></a>
+                <a><i className={`${styles.footerIcon} fab fa-linkedin asdf`}></i></a>
+                <a><i className={`${styles.footerIcon} fab fa-instagram`}></i></a>
+            </section>
+        </footer>
+    )
+}
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Footer from '../components/Footer/Footer';
 
 export default function Home() {
   return (
@@ -7,11 +8,17 @@ export default function Home() {
       <Head>
         <title>Ryan Devenney</title>
         <link rel="icon" href='Favicon.png' />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Yusei+Magic&display=swap" rel="stylesheet" />
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.2/css/all.css" integrity="sha384-vSIIfh2YWi9wW0r9iZe7RJPrKwp6bG+s9QZMoITbCckVJqGCCRhc+ccxNcdpHuYu" crossorigin="anonymous"></link>
       </Head>
 
-      <main className={styles.main}>
-        
+      <main>
+        <h1 className={styles.mainHeader}>Ryan Devenney</h1>
+        <h2 className={styles.subHeader}>Full Stack Developer</h2>
       </main>
+
+      <Footer />
     </div>
   )
 }
