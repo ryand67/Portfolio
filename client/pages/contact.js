d5ce7019a44d6dbf6b3a2cd5d268834feecca8d9
ryand67 <rwdevenney@gmail.com>
2021-01-22 15:36:35 -0500

    favicon, footer, resume

--- client/pages/contact.js
+++ client/pages/contact.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Head from 'next/head';
 import Nav from '../components/NavHeader/NavHeader';
 import Footer from '../components/Footer/Footer';
 import styles from '../styles/contact.module.css';
@@ -22,6 +23,14 @@ export default function contact() {
 
     return (
         <>
+        <Head>
+        <title>Ryan Devenney</title>
+        <link rel="icon" href='faviconNew.png' />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Nanum+Gothic&display=swap" rel="stylesheet"></link>
+        <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;0,800;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet" />
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.2/css/all.css" integrity="sha384-vSIIfh2YWi9wW0r9iZe7RJPrKwp6bG+s9QZMoITbCckVJqGCCRhc+ccxNcdpHuYu" crossorigin="anonymous"></link>
+      </Head>
             <Nav />
             <main className={styles.contact}>
                 <h1 className={styles.contactHeader}>Contact</h1>
--- client/pages/index.js
+++ client/pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>Ryan Devenney</title>
-        <link rel="icon" href='Favicon.png' />
+        <link rel="icon" href='faviconNew.png' />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Nanum+Gothic&display=swap" rel="stylesheet"></link>
         <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;0,800;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet" />
@@ -17,11 +17,10 @@ export default function Home() {
 
       <NavHeader />
       <main className={styles.homeMain}>
-
         <h1 className={styles.mainHeader}>Ryan Devenney</h1>
         <div className={styles.subHeadInfo}>
-          <h2 className={styles.subHeader}>I'm a full stack React and NextJS developer located in Charlotte, NC looking to make your next project a reality.</h2>
-          <h2 className={styles.subHeader}>Check out my past work <a href="/work" className={styles.mainLink}>here</a>.</h2>
+          <h2 className={styles.subHeader}>I'm a full stack React and Next.js developer located in Charlotte, NC looking to make your next project a reality.</h2>
+          <h2 className={styles.subHeader}>Check out my past work <em><a href="/work" className={styles.mainLink}>here</a></em>.</h2>
         </div>
       </main>
 
