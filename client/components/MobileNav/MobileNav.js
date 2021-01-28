e667d570c2a0a1eddfb6634b1d975da47979bf85
ryand67 <rwdevenney@gmail.com>
2021-01-28 12:48:11 -0500

    link change

--- client/components/MobileNav/MobileNav.js
+++ client/components/MobileNav/MobileNav.js
@@ -6,7 +6,7 @@ export default function MobileNav() {
         <nav className={styles.nav}>
             <ul className={styles.navList}>
                 <li className={styles.navListItem}><a href='/'>home</a></li>
-                <li className={styles.navListItem}><a href='ryanDevenneyResumeNew.pdf' target="_blank">about</a></li>
+                <li className={styles.navListItem}><a href='ryanDevenneyResume.pdf' target="_blank">about</a></li>
                 <li className={styles.navListItem}><a href="/work">work</a></li>
                 <li className={styles.navListItem}><a href="/contact">contact</a></li>
             </ul>
