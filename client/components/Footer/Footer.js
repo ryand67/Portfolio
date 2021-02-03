e23e454ec08810965a0e5d93786126096a959fb6
ryand67 <rwdevenney@gmail.com>
2021-02-03 10:37:50 -0500

    footer

--- client/components/Footer/Footer.js
+++ client/components/Footer/Footer.js
@@ -7,7 +7,7 @@ export default function Footer() {
             <section className={styles.footerIcons}>
                 <a href="https://github.com/ryand67" target="_blank"><i className={`${styles.footerIcon} fab fa-github`}></i></a>
                 <a href="https://www.linkedin.com/in/ryandevenney67/" target="_blank"><i className={`${styles.footerIcon} fab fa-linkedin asdf`}></i></a>
-                <a href="https://www.instagram.com/ryan_devenney67/" target="_blank"><i className={`${styles.footerIcon} fab fa-instagram`}></i></a>
+                {/* <a href="https://www.instagram.com/ryan_devenney67/" target="_blank"><i className={`${styles.footerIcon} fab fa-instagram`}></i></a> */}
                 <a href="/contact"><i className={`${styles.footerIcon} fas fa-envelope-square`}></i></a>
             </section>
         </footer>
