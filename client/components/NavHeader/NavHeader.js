b11ef120af2f32c0a9340e459aa8199fe995b2f3
ryand67 <rwdevenney@gmail.com>
2021-03-30 14:36:09 -0400

    nav active

--- client/components/NavHeader/NavHeader.js
+++ client/components/NavHeader/NavHeader.js
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styles from './NavHeader.module.css';
 
 export default function NavHeader() {
+
+    const [path, setPath] = useState('');
+
+    useEffect(() => {
+        setPath(window.location.pathname);
+    }, [])
+
     return (
         <nav className={styles.nav}>
             <ul className={styles.navList}>
-                <li className={styles.navListItem}><a href='/'>home</a></li>
-                <li className={styles.navListItem}><a href="/work">work</a></li>
-                <li className={styles.navListItem}><a href='ryanDevenneyResume.pdf' target="_blank">resume</a></li>
-                <li className={styles.navListItem}><a href="/contact">contact</a></li>
+                <li className={path === '/' ? `${styles.navListItem}` : `${styles.disabled}`}><a href='/'>home</a></li>
+                <li className={path === '/work' ? `${styles.navListItem}` : `${styles.disabled}`}><a href="/work">work</a></li>
+                <li className={styles.disabled}><a href='ryanDevenneyResume.pdf' target="_blank">resume</a></li>
+                <li className={path === '/contact' ? `${styles.navListItem}` : `${styles.disabled}`}><a href="/contact">contact</a></li>
             </ul>
         </nav>
     )
